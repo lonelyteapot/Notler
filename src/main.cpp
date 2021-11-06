73e2eccae9f876c21a8bfb46d86912e22b6d5ec4
Dmitry Semenov <lonelyteapot@gmail.com>
2021-11-07 01:33:10 +0300

    Create NotesListModel

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <src/models/noteslistmodel.h>
 
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
+
+    qmlRegisterType<NotesListModel>("NotlerTypes", 1, 0, "NotesListModel");
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
